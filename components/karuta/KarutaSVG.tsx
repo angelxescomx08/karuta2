ce80673a7952a81e00f7350770b3326174fb65b0
angelxescomx08 <ah@xdevelop.mx>
2022-07-16 16:02:15 -0500

    animacion en transition en las cartas

--- components/karuta/KarutaSVG.tsx
+++ components/karuta/KarutaSVG.tsx
@@ -1,16 +1,20 @@
 import { FC } from "react"
+import { useTheme } from '@mui/material/styles';
 
 interface Props {
     poema: string;
 }
 
 export const KarutaSVG: FC<Props> = ({ poema }) => {
+    const theme = useTheme();
+    console.log(theme);
     return (
         <svg width="100%" height={400}
             xmlns="http://www.w3.org/2000/svg"
             style={{
-                backgroundColor: '#000',
-                color: '#fff',
+                backgroundColor: theme.palette.mode === 'dark' ? '#000' : '#f1f1f1',
+                color: theme.palette.text.primary,
+                transition: 'background-color .3s ease, color .3s ease'
             }}>
             <switch>
                 <foreignObject width="100%" height="100%">
