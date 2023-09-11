23d875326c942e85cc70eef9ab91591122139666
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-09-10 20:15:18 -0600

    metadata y loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,32 @@ const roboto = Roboto({
   subsets: ['latin']
 })
 
-export const metadata: Metadata = {
-  title: 'Karuta App',
-  description: 'App to see all the different poems of Karuta sport, Chihayafuru anime',
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Karuta App',
+    description: 'App to see all the different poems of Karuta sport, Chihayafuru anime',
+    metadataBase: new URL('https://karuta.vercel.app'),
+    authors: {
+      name: "Jose Angel Hernandez Radilla",
+      url: "https://www.jose-angel-hdz-rda.com/"
+    },
+    creator: "Jose Angel Hernandez Radilla",
+    keywords: ["Karuta","Card","Chihayafuru","Anime","Sport"],
+    openGraph: {
+      title: `Karuta App`,
+      description: 'App to see all the different poems of Karuta sport, Chihayafuru anime',
+      siteName: "Karuta App",
+      images: "/assets/imgs/chihayafuru.jpg"
+    },
+    twitter: {
+      title: `Karuta App`,
+      description: `App to see all the different poems of Karuta sport, Chihayafuru anime`,
+      images: "/assets/imgs/chihayafuru.jpg",
+      card: 'summary_large_image',
+      creator: "Jose Angel Hernandez Radilla",
+    }
+  }
 }
 
 export default function RootLayout ({
---
+++ app/layouts/CardLayout.tsx
@@ -0,0 +1,13 @@
+import React, { type PropsWithChildren, type ReactElement } from 'react'
+import { HeaderSimple } from './ui/header-simple'
+
+export const CardLayout = ({children}: PropsWithChildren): ReactElement => {
+  return (
+    <>
+      <HeaderSimple />
+      <main className='grid grid-cols-12 container m-auto gap-5 py-5 px-2'>
+        {children}
+      </main>
+    </>
+  )
+}
