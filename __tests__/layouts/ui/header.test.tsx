884205af1cb04bc70657c320daa687770bbf3044
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-10-31 23:27:04 -0600

    header test

---
+++ __tests__/layouts/ui/header.test.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/app/layouts/ui/header";
+import { createTheme, MantineProvider } from "@mantine/core";
+import { render, screen } from "@testing-library/react";
+
+const theme = createTheme({});
+
+describe("Header", () => {
+  it("should render the header", () => {
+    render(
+      <MantineProvider theme={theme}>
+        <Header />
+      </MantineProvider>
+    );
+    expect(screen.getByText("Karuta App")).toBeDefined();
+  });
+});
---
+++ jest.setup.ts
@@ -0,0 +1,38 @@
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// setupTests.js
+class MockResizeObserver {
+  callback: (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+
+  constructor(callback: 
+    (entries: ResizeObserverEntry[], observer: ResizeObserver) => void
+  ) {
+    this.callback = callback;
+  }
+
+  observe() {
+    // Simula la observación sin hacer nada
+  }
+
+  unobserve() {
+    // Simula detener la observación sin hacer nada
+  }
+
+  disconnect() {
+    // Simula la desconexión
+  }
+}
+
+global.ResizeObserver = MockResizeObserver;
