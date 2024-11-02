0487e1ff1b57ea5dce93b5ca4dd793e837ed2e79
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-11-01 18:13:44 -0600

    prueba de search input

---
+++ __tests__/components/inputs/search-input.test.tsx
@@ -0,0 +1,23 @@
+
+import { SearchInput } from "../../../app/components/inputs/serach-input";
+import { MantineProvider } from "@mantine/core";
+import { render } from "@testing-library/react";
+
+// Haz un mock del componente SearchInput
+jest.mock("../../../app/components/inputs/serach-input", () => ({
+  SearchInput: jest.fn(() => <div>SearchInput Mock</div>),
+}));
+
+describe('SearchInput', () => {
+  it('should render correctly', () => {
+    // Renderiza el componente dentro de MantineProvider
+    render(
+      <MantineProvider>
+        <SearchInput />
+      </MantineProvider>
+    );
+
+    // Verifica que SearchInput fue llamado
+    expect(SearchInput).toHaveBeenCalled();
+  });
+});
