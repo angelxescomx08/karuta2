6c30949a86b87fcd137513202164ef4e02a0668b
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-11-01 18:21:58 -0600

    snapshot

---
+++ __tests__/components/skeletons/card-page-skeleton.test.tsx
@@ -0,0 +1,14 @@
+import CardPageSkeleton from "@/app/components/skeletons/card-page-skeleton"
+import { MantineProvider } from "@mantine/core"
+import { render } from "@testing-library/react"
+
+describe('CardPageSkeleton', () => {
+  it('renders correctly', () => {
+    const { asFragment } = render(
+      <MantineProvider>
+        <CardPageSkeleton />
+      </MantineProvider>
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
